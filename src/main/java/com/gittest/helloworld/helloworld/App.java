4bb7400834e93319de930e88e1af2f7b10adadfe
Robert Rhyne <robert_rhyne@premierinc.com>
2013-09-12 14:19:17 -0400

    Added newer line

--- src/main/java/com/gittest/helloworld/helloworld/App.java
+++ src/main/java/com/gittest/helloworld/helloworld/App.java
@@ -4,12 +4,11 @@ package com.gittest.helloworld.helloworld;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
         System.out.println( "Testing" );
         System.out.println( "New Line" );
+        System.out.println( "Newer Line" );
     }
 }
